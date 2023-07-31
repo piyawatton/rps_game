0660e6ef8ca483b89e8d1cf6a97b4cfecf7f1a44
Piyawat <piyawat.chutimavutikul@gmail.com>
2023-08-01 00:04:46 +0700

    fix query ranking

--- pages/api/score.ts
+++ pages/api/score.ts
@@ -7,34 +7,34 @@ import { UserInfo } from '@/src/type/User';
 import db from '@/src/utils/db';
 import { TOP_N_HIGH_SCORE } from '@/src/config/global';
 
-export async function handler(req: NextApiRequest, res: NextApiResponse<BaseResponse<Score[]>>) {
-  async function getHighestScoreWithRanking() {
-    try {
-      const query = `
-        SELECT scores.user_id, scores.score, scores.created_at,
-        RANK() OVER (ORDER BY scores.score DESC, scores.created_at ASC) AS ranking,
-              u.name
-        FROM "Score" scores 
-        JOIN "User" u ON scores.user_id = u.id
-        WHERE scores.score > 0
-        LIMIT ${TOP_N_HIGH_SCORE};
-      `;
+async function getHighestScoreWithRanking() {
+  try {
+    const query = `
+      SELECT scores.user_id, scores.score, scores.created_at,
+      RANK() OVER (ORDER BY scores.score DESC, scores.created_at ASC) AS ranking,
+            u.name
+      FROM "Score" scores 
+      JOIN "User" u ON scores.user_id = u.id
+      WHERE scores.score > 0
+      LIMIT ${TOP_N_HIGH_SCORE};
+    `;
 
-      const results = await db.any(query);
-      return results;
-    } catch (error) {
-      console.error('Error getting highest score with ranking:', error);
-      throw error;
-    }
+    const results = await db.any(query);
+    return results;
+  } catch (error) {
+    console.error('Error getting highest score with ranking:', error);
+    throw error;
   }
+}
+export async function handler(req: NextApiRequest, res: NextApiResponse<BaseResponse<Score[]>>) {
   const globalHighScore: (Score & UserInfo)[] = await getHighestScoreWithRanking();
   if (globalHighScore) {
-    res.json({
+    return res.json({
       success: true,
       data: globalHighScore || [],
     })
   }
-  res.json({
+  return res.json({
     success: false,
     message: 'Something went wrong'
   })
